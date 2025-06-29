6e687ea93627e55429c8a4de5af593c1c9eb2d7d
Henil28P <henilp467@gmail.com>
2025-06-29 18:43:39 +1000

    [main.py]: implement function to load and preprocess the plant image using PIL

--- App/main.py
+++ App/main.py
@@ -13,3 +13,12 @@ model = tf.keras.models.load_model(model_path)
 
 # loading the class names
 class_indices = json.load(open(f"{current_dir}/class_indices.json"))
+
+# Function to load and preprocess the Image using Pillow
+def load_and_preprocess_image(image_path, target_size=(224, 224)):
+    img = Image.open(image_path) # load the image
+    img = img.resize(target_size) # resize the image
+    img_array = np.array(img) # convert the image to a numpy array
+    img_array = np.expand_dims(img_array, axis=0) # add batch dimension
+    img_array = img_array.astype('float32') / 255. # scale the image values to [0,1]
+    return img_array
