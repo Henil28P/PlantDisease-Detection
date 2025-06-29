9353f6d52f69dd4984e12dd721faee5c79f1f05e
Henil28P <henilp467@gmail.com>
2025-06-29 18:21:17 +1000

    [main.py]: define model path and directory/files structures

--- App/main.py
+++ App/main.py
@@ -5,3 +5,11 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 import streamlit as st
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+model_path = f"{current_dir}/Trained Model/plant_disease_prediction_model.h5"
+# Load the pre-trained model
+model = tf.keras.models.load_model(model_path)
+
+# loading the class names
+class_indices = json.load(open(f"{current_dir}/class_indices.json"))
